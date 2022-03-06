6bfa5a4c2276e4e609693aca2a808f63f0eb7c46
khaifahmi99 <khaifahmi99@github.com>
2022-03-06 12:24:06 +1100

    add more styling to the navbar
    make it sticky and show bottom border

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return(
-    <div className="w-full h-16 bg-sky-900">
+    <div className="w-full bg-sky-900 sticky top-0" style={{ borderBottom: '4px solid purple' }}>
       <div className="flex flex-row items-center justify-between mx-8">
         <div className='w-12 h-12 p-2 text-white hover:cursor-pointer' onClick={() => navigate('/')}>
           <BookOpenIcon />
