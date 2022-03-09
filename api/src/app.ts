75203862bd3b489f576944a99055300480df23f1
khaifahmi99 <khaifahmi99@github.com>
2022-03-09 20:48:36 +1100

    enable cors *

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,17 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { Client } from './client';
 import bodyParser from 'body-parser';
-import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: '*'
-}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
+
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (_, res) => {
   res.send('Hello world!');
